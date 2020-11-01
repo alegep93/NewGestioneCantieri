ebf61948103b224ad81f23286d008d0e3b73eeb6
Alessandro Geppi <geppi.alessandro@gmail.com>
2020-11-01 12:00:43 +0100

    Nuovo fix stampa excel per simboli di valuta

--- GestioneCantieri/Utils/ExcelManager.cs
+++ GestioneCantieri/Utils/ExcelManager.cs
@@ -14,18 +14,20 @@ namespace GestioneCantieri.Utils
         {
             StringWriter strwritter = new StringWriter();
             HtmlTextWriter htmltextwrtter = new HtmlTextWriter(strwritter);
+            string style = @"<style> .textmode {mso-number-format:General} </style>";
             response.Clear();
             response.Buffer = true;
             response.ClearContent();
             response.ClearHeaders();
             response.Charset = "";
+            response.ContentEncoding = System.Text.Encoding.Unicode; /*c# uses UTF-16 internally*/
+            response.BinaryWrite(System.Text.Encoding.Unicode.GetPreamble());
             fileName += ".xls";
             response.Cache.SetCacheability(HttpCacheability.NoCache);
-            response.ContentType = "application/vnd.ms-excel";
             response.AddHeader("Content-Disposition", "attachment;filename=" + fileName);
+            response.ContentType = "application/ms-excel";
             gridView.GridLines = GridLines.Both;
             gridView.HeaderStyle.Font.Bold = true;
-            string style = @"<style> td { mso-number-format:\@;} </style>";
             response.Write(style);
             gridView.RenderControl(htmltextwrtter);
             response.Write(strwritter.ToString());
